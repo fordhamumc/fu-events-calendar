a98907c0d6d4a6c101baf45859866c9bf5019877
Michael Foley <michaeldfoley@gmail.com>
2017-11-09 17:35:07 -0500

    customized rss feed for iModules pages

--- fu-events-calendar.php
+++ fu-events-calendar.php
@@ -170,3 +170,79 @@ function fu_meta_query( $q ) {
   }
 };
 add_action('tribe_events_parse_query', 'fu_meta_query');
+
+
+/**
+ * Remove Country from Venue if United States
+ *
+ * @author Michael Foley
+ *
+ * @var array   $venue_details
+ *
+ * @return array
+ *
+ */
+
+function remove_united_states_venue($venue_details){
+  $venue_details["address"] = str_replace("United States", "", $venue_details["address"]);
+  return $venue_details;
+}
+add_filter( 'tribe_get_venue_details', 'remove_united_states_venue' );
+
+
+/**
+ * Customize the description in the RSS feed for
+ * iModules pages
+ *
+ * @author Michael Foley
+ *
+ * @var string   $excerpt
+ *
+ * @return string
+ *
+ */
+
+function custom_imods_event_feed($excerpt){
+  global $post;
+
+  $format = filter_input( INPUT_GET, 'imods', FILTER_SANITIZE_STRING );
+
+  if ( is_object( $post ) && $post->post_type == TribeEvents::POSTTYPE && is_feed() && $format ) {
+    $month = tribe_get_start_date($post->ID, false, 'M');
+    $day = tribe_get_start_date($post->ID, false, 'j');
+    $time = tribe_get_start_time();
+    $time = str_replace( array(":00", "am", "pm"), array("", "a.m.", "p.m."), $time );
+    $date = $start_date = tribe_get_start_date($post->ID, false, 'l, F j, Y');
+    $end_date = tribe_get_end_date($post->ID, false, 'l, F j, Y');
+    $title = tribe_get_event_link($post->ID, true);
+    $location = tribe_get_venue();
+
+    if ( !empty(tribe_get_address()) ) {
+      $location .=  ', ' . tribe_get_venue_details()['address'];
+    }
+
+    if ($start_date !== $end_date) {
+      $date = (substr($start_date, -4) === substr($end_date, -4)) ? substr($start_date, 0, -6) : $start_date;
+      $date .= " &ndash; " . $end_date;
+    }
+
+    if ( $format === "2" || $format === "long" ){
+      return <<<RSS
+<h3>{$title}</h3>
+<p>{$date}<br />{$location}</p>
+RSS;
+
+    } else {
+      return <<<RSS
+<div class="date-block">{$month} <span>{$day}</span></div>
+<div class="event-block">
+  <h3>{$title}</h3>
+  <div class="event-time">{$time}</div>
+</div>
+RSS;
+    }
+  }
+
+  return $excerpt;
+}
+add_filter( 'the_excerpt_rss', 'custom_imods_event_feed' );
