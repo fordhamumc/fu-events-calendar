07b8660d2e1044ffc9f93e0bb7935d7f8e92b6db
Michael Foley <michaeldfoley@gmail.com>
2017-12-07 15:55:29 -0500

    updated class name for widget date

--- admin/widgets/related.php
+++ admin/widgets/related.php
@@ -28,17 +28,19 @@ class Fu_Tec_Related_Widget extends WP_Widget {
 
     <ul class="tab-related-events posts-list">
       <?php foreach ( $posts as $post ) : ?>
-      <li>
+      <li class="<?php tribe_events_event_classes() ?>">
         <?php $thumb = ( has_post_thumbnail( $post->ID ) ) ? get_the_post_thumbnail( $post->ID, 'thumbnail' ) : '<img src="' . esc_url( trailingslashit( Tribe__Events__Pro__Main::instance()->pluginUrl ) . 'src/resources/images/tribe-related-events-placeholder.png' ) . '" alt="' . esc_attr( get_the_title( $post->ID ) ) . '" />'; ?>
 
         <a href="<?php echo esc_url( tribe_get_event_link( $post ) ); ?>" class="url" rel="bookmark"><?php echo $thumb ?></a>
         <div class="content">
           <?php
           if ( $post->post_type == Tribe__Events__Main::POSTTYPE ) {
-            echo tribe_events_event_schedule_details( $post, '<div class="tribe-events-related-widget-date">', '</div>' );
+            echo tribe_events_event_schedule_details( $post, '<div class="tribe-events-widget-date">', '</div>' );
           }
           ?>
-          <a href="<?php echo tribe_get_event_link( $post ); ?>" class="tribe-event-url" rel="bookmark"><?php echo get_the_title( $post->ID ); ?></a>
+          <h4 class="tribe-events-title">
+            <a href="<?php echo tribe_get_event_link( $post ); ?>" class="tribe-event-url" rel="bookmark"><?php echo get_the_title( $post->ID ); ?></a>
+          </h4>
         </div>
       </li>
       <?php endforeach; ?>
